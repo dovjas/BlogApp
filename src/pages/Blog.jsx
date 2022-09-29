2f2d955944fe4cde8e76a3fce5e9639f1fb96533
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-09-29 20:02:49 +0300

    created - single blog page design

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className="blog">
+      <div className="content">
+        <img src="https://designshack.net/wp-content/uploads/weighcomm.jpg"/>
+      <div className="user">
+        <img src="https://designshack.net/wp-content/uploads/uglydrinks.jpg" />
+      <div className="info">
+        <span>Jonas</span>
+        <p>Posted 2 days ago</p>
+      </div>
+      </div>
+      <h1>Lorem ipsum title</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ipsum, ex aliquam explicabo ratione laborum quo optio corporis laboriosam alias non sit soluta error in, repellat laudantium asperiores quas quia.</p>
+      </div>
+      <div className="menu">M</div>
+    </div>
   )
 }
 
