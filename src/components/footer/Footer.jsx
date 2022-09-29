31b914081cbc75bb40eaf3e46fb87512aeb9004c
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-09-29 16:23:50 +0300

    created - basic footer style

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <h2>LOGO</h2>
+      <span>
+        Made by <b>DJ</b>
+      </span>
+    </footer>
   )
 }
 
