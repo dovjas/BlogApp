785f2add4cb117156eaf621b55bed221bb851cdf
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-09-29 16:15:39 +0300

    created - navbar simple style

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import {Link} from "react-router-dom";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className="container">
+        <div className="logo">LOGO</div>
+        <div className="links">
+          <Link className="link" to="/">
+            <h4>Home</h4>
+          </Link>
+          <Link className="link" to="/">
+            <h4>Home</h4>
+          </Link>
+          <Link className="link" to="/">
+            <h4>Home</h4>
+          </Link>
+          <Link className="link" to="/">
+            <h4>Home</h4>
+          </Link>
+          <Link className="link" to="/">
+            <h4>Home</h4>
+          </Link>
+            <p>userX</p>
+            <span>Logout</span>
+            <span className="create-new">
+              <Link to="/createBlog" className="link">Create New</Link>
+            </span>
+        </div>
+      </div>
+    </nav>
+
   )
 }
 
